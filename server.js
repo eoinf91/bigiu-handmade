8920fbe396983e705b32822ced904b5983049687
Eoin <me@eoinfeely.com>
2020-07-06 15:26:02 +0100

    updating stripe currency to Euro

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
+    currency: 'eur'
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
